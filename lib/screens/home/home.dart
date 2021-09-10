98c206545193fd8594b9f0742ea734c150fc6be2
Elharith Elsheikh <elharith.elsheikh@ericsson.com>
2021-09-10 22:45:18 +0200

    Solved Dashboard Page Issues

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,96 +1,137 @@
 import 'package:flutter/material.dart';
-import 'package:distribution/services/auth.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:distribution/services/auth.dart';
 
-class Home extends StatelessWidget {
+List<StaggeredTile> _staggeredTiles = const <StaggeredTile>[
+  StaggeredTile.extent(2, 150),
+  StaggeredTile.extent(1, 170),
+  StaggeredTile.extent(1, 170),
+  StaggeredTile.extent(2, 150)
+];
+
+List<Widget> _tiles = const <Widget>[
+  const MyItems(Icons.done_all, "Sell A Product", 0xffed622b, "/first"),
+  const MyItems(Icons.people_rounded, "My Customers", 0xff26cb3c, "/second"),
+  const MyItems(
+      Icons.account_circle_outlined, "My Profile", 0xffff3266, "Third"),
+  const MyItems(Icons.money, "Sales Archive", 0xff4527a0, "Fourth"),
+];
+
+class DashboardItems extends StatelessWidget {
   AuthService _auth = AuthService();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Home'),
+          actions: <Widget>[
+            FlatButton.icon(
+              icon: Icon(Icons.person),
+              label: Text("Logout"),
+              onPressed: () async {
+                await _auth.signOut();
+              },
+            )
+          ],
+        ),
+        body: StaggeredGridView.count(
+          crossAxisCount: 2,
+          staggeredTiles: _staggeredTiles,
+          children: _tiles,
+          mainAxisSpacing: 12.0,
+          crossAxisSpacing: 12.0,
+          padding: EdgeInsets.symmetric(horizontal: 16, vertical: 90),
+        ));
+  }
+}
 
-  Material MyItems(IconData icon, String title, int color) {
+class MyItems extends StatelessWidget {
+  const MyItems(this.icon, this.heading, this.color, this.routeName);
+
+  final int color;
+  final IconData icon;
+  final String heading;
+  final String routeName;
+
+  @override
+  Widget build(BuildContext context) {
     return Material(
       color: Colors.white,
       elevation: 14.0,
       shadowColor: Color(0x802196F3),
       borderRadius: BorderRadius.circular(24.0),
       child: InkWell(
-        onTap: () {},
-        child: Center(
-          child: Padding(
-              padding: EdgeInsets.all(9),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Center(
-                        child: Padding(
-                          padding: EdgeInsets.all(9),
-                          child: Text(
-                            title,
-                            style: TextStyle(
-                              color: new Color(color),
-                              fontSize: 20,
+          onTap: () {
+            Navigator.pushNamed(context, routeName);
+          },
+          child: Center(
+              child: Padding(
+                  padding: EdgeInsets.all(9),
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Center(
+                              child: Padding(
+                                padding: EdgeInsets.all(9),
+                                child: Text(
+                                  heading,
+                                  style: TextStyle(
+                                    color: new Color(color),
+                                    fontSize: 20,
+                                  ),
+                                ),
+                              ),
                             ),
-                          ),
+
+                            //icon
+                            Material(
+                              color: new Color(color),
+                              borderRadius: BorderRadius.circular(24.0),
+                              child: Padding(
+                                padding: const EdgeInsets.all(16.0),
+                                child: Icon(
+                                  icon,
+                                  size: 30,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
-                      ),
-                      Material(
-                        color: new Color(color),
-                        borderRadius: BorderRadius.circular(24),
-                        child: Padding(
-                          padding: const EdgeInsets.all(16),
-                          child: Icon(
-                            icon,
-                            color: Colors.white,
-                            size: 30,
-                          ),
-                        ),
-                      )
-                    ],
-                  )
-                ],
-              )),
-        ),
-      ),
+                      ])))),
     );
   }
+}
 
+class Home extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Home", style: TextStyle(color: Colors.white)),
-        centerTitle: true,
-        elevation: 0.0,
-        backgroundColor: Colors.blue[200],
-        actions: <Widget>[
-          FlatButton.icon(
-            icon: Icon(Icons.person),
-            label: Text("Logout"),
-            onPressed: () async {
-              await _auth.signOut();
-            },
-          )
-        ],
-      ),
-      body: StaggeredGridView.count(
-        crossAxisCount: 2,
-        crossAxisSpacing: 12,
-        mainAxisSpacing: 12,
-        padding: EdgeInsets.symmetric(horizontal: 16, vertical: 90),
-        children: <Widget>[
-          MyItems(Icons.done_all, "Sell A Product", 0xffed622b),
-          MyItems(Icons.people_rounded, "My Customers", 0xff26cb3c),
-          MyItems(Icons.account_circle_outlined, "My Profile", 0xffff3266),
-          MyItems(Icons.money, "Sales Archive", 0xff4527a0),
-        ],
-        staggeredTiles: [
-          StaggeredTile.extent(2, 130),
-          StaggeredTile.extent(1, 150),
-          StaggeredTile.extent(1, 150),
-          StaggeredTile.extent(2, 130)
-        ],
-      ),
+    return MaterialApp(
+      initialRoute: '/',
+      routes: {
+        // When navigating to the "/" route, build the FirstScreen widget.
+        '/': (context) => DashboardItems(),
+        '/first': (context) => FirstScreen(),
+        '/second': (context) => SecondScreen(),
+      },
     );
+  }
+}
+
+class FirstScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Text("First Screen");
+  }
+}
+
+class SecondScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Text("Second Screen");
   }
 }
