e182be7f3782ea81b43c5f9787a32a14f20490c8
Elharith Elsheikh <elharith.elsheikh@ericsson.com>
2021-09-13 12:29:40 +0200

    completed the first stage of routing user to another screen

--- lib/screens/home/sell_category/getcustomer.dart
+++ lib/screens/home/sell_category/getcustomer.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:distribution/shared/constant.dart';
@@ -11,6 +13,17 @@ class _GetCustomerState extends State<GetCustomer> {
   final _formkey = GlobalKey<FormState>();
   String phone_number = "";
   String error = "";
+  String check_number = "";
+
+  String validateMobile(String value) {
+    String pattern = r'(^(?:[0]9)?[0-9]{10}$)';
+    RegExp regExp = new RegExp(pattern);
+
+    if (!regExp.hasMatch(value)) {
+      return 'Please enter valid mobile number';
+    }
+    return null;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -44,24 +57,38 @@ class _GetCustomerState extends State<GetCustomer> {
               child: Text("Enter", style: TextStyle(color: Colors.white)),
               onPressed: () async {
                 if (_formkey.currentState.validate()) {
-                  try {
-                    FirebaseFirestore.instance
-                        .collection('customers')
-                        .where('phone_number', isEqualTo: phone_number)
-                        .get()
-                        .then((QuerySnapshot querySnapshot) {
-                      if (querySnapshot.size == 0) {
-                        print("There are No data ");
-                      } else {
-                        querySnapshot.docs.forEach((doc) {
-                          print(doc["phone_number"]);
-                          print(doc["name"]);
-                        });
-                      }
-                    }).catchError(
-                            (error) => print("Failed to add user: $error"));
-                  } catch (e) {
-                    print("Can't fetch the data");
+                  check_number = validateMobile(phone_number);
+                  print(check_number);
+                  if (check_number != null) {
+                    error = check_number;
+                  } else {
+                    error = "";
+                    try {
+                      FirebaseFirestore.instance
+                          .collection('customers')
+                          .where('phone_number', isEqualTo: phone_number)
+                          .get()
+                          .then((QuerySnapshot querySnapshot) {
+                        if (querySnapshot.size == 0) {
+                          print("There are No data ");
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => register_customer()));
+                        } else {
+                          querySnapshot.docs.forEach((doc) {
+                            print(doc["phone_number"]);
+                            print(doc["name"]);
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => sell_customer()));
+                          });
+                        }
+                      });
+                    } catch (e) {
+                      print("Can't fetch the data");
+                    }
                   }
                 }
               },
@@ -73,6 +100,39 @@ class _GetCustomerState extends State<GetCustomer> {
             )
           ]),
         ),
+      ),
+    );
+  }
+}
+
+class sell_customer extends StatefulWidget {
+  @override
+  _sell_customerState createState() => _sell_customerState();
+}
+
+class _sell_customerState extends State<sell_customer> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Registering a sale operation"),
+      ),
+    );
+  }
+}
+
+class register_customer extends StatefulWidget {
+  @override
+  _register_customerState createState() => _register_customerState();
+}
+
+class _register_customerState extends State<register_customer> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Customer Registeration Form"),
+        centerTitle: true,
       ),
     );
   }
